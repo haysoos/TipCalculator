948d4a7a3cb150fcaa595e1ccaa80e77d4f040eb
jmedrano <jmedrano@yahoo-inc.com>
2013-05-29 17:33:04 -0700

    Working version

--- TipCalculator/src/medrano/jesus/tipcalculator/MainActivity.java
+++ TipCalculator/src/medrano/jesus/tipcalculator/MainActivity.java
@@ -3,6 +3,10 @@ package medrano.jesus.tipcalculator;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -19,4 +23,30 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+
+	public void tenPercentClicked(View view){
+		double percent = 0.10;
+		updateTipTextView(percent);
+	}
+
+	public void fifteenPercentClicked(View view){
+		double percent = 0.15;
+		updateTipTextView(percent);
+
+	}
+	
+	public void twentyPercentClicked(View view){
+		double percent = 0.20;
+		updateTipTextView(percent);
+
+	}
+
+	private void updateTipTextView(double percent) {
+		EditText editText = (EditText) this.findViewById(R.id.editText1);
+		TextView textView = (TextView) this.findViewById(R.id.textView1);
+		double userInput = Double.parseDouble(editText.getText().toString());
+		double tip = userInput * percent;
+		textView.setText(getString(R.string.tipTextViewDefault) + String.format("%.2f", tip));
+	}
+	
 }
